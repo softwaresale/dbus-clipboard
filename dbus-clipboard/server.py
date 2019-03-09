c85a9000856b5fe8c99c2dc522171942d924499b
Charlie Sale <softwaresale01@gmail.com>
2019-03-08 21:33:59 -0500

    dbus-clipboard: server: Finished obj introspection
    Clipboard server introspection object is up to date with completed
    methods.

--- dbus-clipboard/server.py
+++ dbus-clipboard/server.py
@@ -5,18 +5,41 @@ from clipstack import ClipStack, Clip
 
 
 class ClipboardServer:
-
+    """
+    <node>
+        <interface name="net.softwaresale.dbus-clipboard.Clipboard">
+            <method name="add_clip">
+                <arg name="mime" type="s" direction="in" />
+                <arg name="data" type="ay" direction="in" />
+                <arg name="restrict" type="b" direction="in" />
+            </method>
+            <method name="first_clip">
+                <arg name="mime" type="s" direction="in" />
+                <arg name="data" type="ay" direction="out" />
+            </method>
+            <method name="pop">
+                <arg name="mime" type="s" direction="in" />
+                <arg name="data" type="ay" direction="out" />
+            </method>
+        </interface>
+    </node>
+    """
     def __init__(self):
         self.stack = ClipStack()
 
     def add_clip(self, mime, data, restrict=None):
+        """
+        Adds a clip object to clipboard.
+        :param
+        """
         newclip = Clip(mime, data)
         self.stack.add(newclip, restrict)
 
     def first_clip(self, mime=None):
-        if mime is not None:
-            for clip in self.stack:
-                if clip.mimetype == mime:
-                    return clip
-        else:
-            return self.stack.pop()
+        clip = self.stack.find_first(mime)
+        if clip is not None:
+            # Return serialized form
+            return clip.data
+
+    def pop(self, mime):
+        return self.stack.pop(len(self.stack) - 1, mime).data
